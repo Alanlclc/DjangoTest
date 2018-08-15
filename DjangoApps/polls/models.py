db4c9da69f9f78050cfff3833de1e868052c76b3
LuChen <M13915061610@163.com>
2018-08-15 15:40:54 +0800

    init

---
+++ DjangoApps/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    Question_text = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+
+    objects = models.Manager()
+    def __str__(self):
+        return self.Question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete = models.CASCADE)
+    Choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.Choice_text
